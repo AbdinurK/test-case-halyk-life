f301a83f86719a4b2733269cbcfefb9f9843a945
Abdinur Kuatbek <kuatbekabdinur@gmail.com>
2023-01-25 16:50:57 +0600

    fix: main

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,4 +1,5 @@
 import { defineStore } from 'pinia'
+import axios from '../plugins/axios'
 
 
 interface LoginFormData {
@@ -19,21 +20,13 @@ export const userStore = defineStore('user', {
     actions: {
         async login(data: LoginFormData, success: () => void, error: (error: any) => void) {
             try {
-                const res  = await fetch(`${import.meta.env.VITE_HALYK_LIFE_TEST_ENDPOINT}/insis/identity/api/Account/login`, {
-                    method: 'POST',
-                    body: JSON.stringify(data),
-                    headers: {
-                        Accept: 'application/json',
-                        'Content-Type': 'application/json',
-                    },
-                })
-                const response = await res.json()
-                if (res.ok) {
-                    localStorage.setItem('accessToken', response.accessToken)
-                    localStorage.setItem('refreshToken', response.refreshToken)
+                const res = await axios.post('/insis/identity/api/Account/login', data)
+                if (res.data) {
+                    localStorage.setItem('accessToken', res.data.accessToken)
+                    localStorage.setItem('refreshToken', res.data.refreshToken)
                     success()
                 } else {
-                    error(response)
+                    error(res.data)
                 }
             } catch (err) {
                 error(err)
